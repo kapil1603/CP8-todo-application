0d760d8b6b63ce17da25a1ff7e9bf5a1d2c0794c
kapil1603 <kapilkumar1603@gmail.com>
2023-03-31 10:43:20 +0000

    CP8-todo-application

--- app.js
+++ app.js
@@ -96,10 +96,48 @@ app.post("/todos/", async (request, response) => {
 
 app.put("/todos/:todoId/", async (request, response) => {
   const { todoId } = request.params;
-  const { status } = request.body;
-  const createTodoId = `
-    INSERT INTO todo (id, todo, priority, status)
-    VALUES  (${id},'${todo}','${priority}','${status}')`;
-  const createId = await db.run(createTodoId);
-  response.send("Todo Successfully Added");
+  let updatedColumn = "";
+  const requestBody = request.body;
+
+  switch (true) {
+    case requestBody.status !== undefined:
+      updatedColumn = "Status";
+      break;
+
+    case requestBody.priority !== undefined:
+      updatedColumn = "Priority";
+      break;
+
+    case requestBody.todo !== undefined:
+      updatedColumn = "Todo";
+      break;
+  }
+
+  const previousTodoId = `
+    SELECT * FROM todo WHERE id = ${todoId}`;
+  console.log(previousTodoId); // SELECT * FROM todo WHERE id = 2
+  const previousTodo = await db.get(previousTodoId);
+  console.log(previousTodo);
+  // { id: 2, todo: 'Some task', priority: 'HIGH', status: 'DONE' }
+  const {
+    todo = previousTodo.todo,
+    status = previousTodo.status,
+    priority = previousTodo.priority,
+  } = request.body;
+
+  const updateTodo = `
+    UPDATE todo 
+    SET todo = '${todo}',priority='${priority}',status='${status}'`;
+  await db.run(updateTodo);
+  response.send(`${updatedColumn} Updated`);
 });
+
+app.delete("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const getTodoId = `
+    DELETE FROM todo WHERE id = ${todoId}`;
+  await db.run(getTodoId);
+  response.send("Todo Deleted");
+});
+
+module.exports = app;
